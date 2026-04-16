5b7d23d117609825b19159f35a5e9d0e0854f608
Scott Mansfield <scott.mansfield@gmail.com>
2026-04-16 17:15:29 -0400

    Updated ref-trim-uml to create an output file per input uml model file.

--- ref-trim-uml.sh
+++ ref-trim-uml.sh
@@ -9,5 +9,7 @@ glob=`find ${basedir} -name *.uml`
 
 for f in $glob
   do
-    xmllint --xpath '//*[local-name()="Reference"]/@reference' ${f} | sed 's/.*reference=//g' | sed 's/\;/\n/g' | sed 's/\"//g' | sed 's/^ *//g'
+    xmllint --xpath '//*[local-name()="Reference"]/@reference' ${f} | sed 's/.*reference=//g' | sed 's/\;/\n/g' | sed 's/\"//g' | sed 's/^ *//g' > ${f}.raw
+    cat ${f}.raw | sort | uniq >${f}.out
+    rm ${f}.raw
   done
